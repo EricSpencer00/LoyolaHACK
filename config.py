f92ed1a97e3d797cea29c68b7b566bae378c231b
ericspencer00 <espencer2@luc.edu>
2025-02-15 11:11:52 -0600

    configs

--- config.py
+++ config.py
@@ -9,8 +9,15 @@ class Config:
     CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL") or "redis://localhost:6379/0"
     CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND") or "redis://localhost:6379/0"
 
+# Email configuration
 MAIL_SERVER = 'smtp.gmail.com'
 MAIL_PORT = 587
 MAIL_USERNAME = os.getenv('MAIL_USERNAME')
 MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
+
+# Firebase configuration
+FB_API_KEY = os.getenv('FB_API_KEY')
+FB_PROJECT_ID = os.getenv('FB_PROJECT_ID')
+FB_SENDER_ID = os.getenv('FB_SENDER_ID')
+FB_APP_ID = os.getenv('FB_APP_ID')
